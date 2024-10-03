1035d6ae4336d41fd0a700068b15ed711e1cd684
Manan Popat <156408695+MananPopat7@users.noreply.github.com>
2024-10-03 10:42:23 +0530

    Add semaphore file and sonarqube (#32)
    * Add semaphore file and sonarqube
    
    * Rename Jenkinsfile to Jenkinsfile.disable
    
    * Update StructEventTest.java

--- src/test/java/com/splunk/kafka/connect/StructEventTest.java
+++ src/test/java/com/splunk/kafka/connect/StructEventTest.java
@@ -40,7 +40,7 @@ public class StructEventTest {
                 .put("timestamp", new Date(1524838717123L))
                 .put("children", children);
         final Event event = new RawEvent(struct, null);
-        final String expected = "{\"first_name\":\"fred\",\"count\":1234,\"timestamp\":\"2018-04-27T14:18:37.123+0000\",\"children\":[{\"first_name\":\"Thing 1\",\"age\":4},{\"first_name\":\"Thing 2\",\"age\":7}]}";
+        final String expected = "{\"first_name\":\"fred\",\"count\":1234,\"timestamp\":\"2018-04-27T14:18:37.123+00:00\",\"children\":[{\"first_name\":\"Thing 1\",\"age\":4},{\"first_name\":\"Thing 2\",\"age\":7}]}";
         final String actual = event.toString();
         Assert.assertEquals(expected, actual);
     }
